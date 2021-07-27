1edb5f7e17331777a678938c62d5ddcd37e20319
samkingsly07 <79350032+samkingsly07@users.noreply.github.com>
2021-07-27 12:40:53 +0530

    main-file

---
+++ APPetit-master/APPetit/riders/src/main/java/com/mad/riders/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.mad.riders;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
